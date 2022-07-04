c898486507c36017bfab368b2dc15cc4edac00b1
Dennis Diaz <Diaz3737@gmail.com>
2022-07-03 21:02:30 -0400

    Foundation finished. Needs styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Places from './Places'
 import data from './data'
 
+
 function App() {
   return (
     <div className="App">
@@ -8,9 +9,18 @@ function App() {
         <img className='planet' src={require('./images/planet.png')} alt="" />
         <p>my travel journal.</p>
       </nav>
-      <Places />
+      <Places
+      title={data[0].title}
+      location={data[0].location}
+      googleMapsUrl={data[0].googleMapsUrl}
+      startDate={data[0].startDate}
+      endDate={data[0].endDate}
+      description={data[0].description}
+      imageUrl={data[0].imageUrl}
+       />
     </div>
   );
 }
+
 
 export default App;
--- src/Places.jsx
+++ src/Places.jsx
@@ -1,17 +1,17 @@
 
-export default function Places() {
+export default function Places(props) {
   return (
     <div className="places">
-      <img src="" alt="Photo of " />
+      <img className="location-photo" src={props.imageUrl} alt={props.title} />
       <div className="country">
-        <img src="" alt="" />
-        <p>Country</p>
-        <a href="#">View on Google Maps</a>
+        <img src='' alt="" />
+        <p className="location">{props.location}</p>
+        <a href={props.googleMapsUrl}>View on Google Maps</a>
       </div>
-      <h1 className="location">Location</h1>
+      <h1 className="title">{props.title}</h1>
       <br />
-      <h4 className="time-spent">Start date - end date</h4>
-      <p className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ab vel pariatur nostrum ut repudiandae. Ab veritatis, cum impedit inventore saepe laboriosam nam labore architecto, ullam itaque, earum perferendis provident</p>
+      <h4 className="time-spent">{props.startDate} - {props.endDate}</h4>
+      <p className="info">{props.description}</p>
     </div>
   )
 }
