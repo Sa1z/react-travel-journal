b861780591c53deb1a56a2000a319a1757231458
Dennis Diaz <Diaz3737@gmail.com>
2022-07-04 20:52:03 -0400

    Added some styling.

--- src/Places.jsx
+++ src/Places.jsx
@@ -3,15 +3,17 @@ export default function Places(props) {
   return (
     <div className="places">
       <img className="location-photo" src={props.imageUrl} alt={props.title} />
-      <div className="country">
-        <img src='' alt="" />
-        <p className="location">{props.location}</p>
-        <a href={props.googleMapsUrl}>View on Google Maps</a>
+      <div className="text-block">
+        <div className="map-info">
+          <img src={require('./images/pin.png')} alt="" />
+          <p className="location">{props.location}</p>
+          <a className='google-link' href={props.googleMapsUrl}>View on Google Maps</a>
+        </div>
+        <h1 className="title">{props.title}</h1>
+        <br />
+        <h4 className="time-spent">{props.startDate} - {props.endDate}</h4>
+        <p className="info">{props.description}</p>
       </div>
-      <h1 className="title">{props.title}</h1>
-      <br />
-      <h4 className="time-spent">{props.startDate} - {props.endDate}</h4>
-      <p className="info">{props.description}</p>
     </div>
   )
 }
